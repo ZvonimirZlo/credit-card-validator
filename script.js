158c75dd679c19b69a3f9264995d2c8884a5cc88
ZvonimirZlo <lovrez2@gmail.com>
2024-01-15 17:49:54 +0100

    4c

--- script.js
+++ script.js
@@ -4,28 +4,28 @@ const container = document.getElementById('card');
 const result = document.getElementById('result');
 
 const formatInput = () => {
-    const nums = inputField.value.toString().replace(/[^\d]/g,'');
-  
-    //slices the string in to pieces to form card number
-    let a = nums.slice(0,4);
-    let b = nums.slice(4,8);
-    let c = nums.slice(8,12)
-    let d = nums.slice(12,16);
-    let e = nums.slice(16);
-    
-    //formatting the input number
-    return `${a} ${b} ${c} ${d} ${e}`
-  }
+  const nums = inputField.value.toString().replace(/[^\d]/g, '');
 
-  const formatter = () => {
-    const formattedInput = formatInput(inputField.value);
-    inputField.value = formattedInput;
-  }
+  //slices the string in to pieces to form card number
+  const a = nums.slice(0, 4);
+  const b = nums.slice(4, 8);
+  const c = nums.slice(8, 12)
+  const d = nums.slice(12, 16);
+  const e = nums.slice(16);
 
-//displays result on the screen
-// btn.addEventListener('click', () => {
-//    let text = document.createElement('h1');
-//    text.innerHTML = inputField.value;
-//     result.appendChild(text);
-// }
-// )
+  //formatting the input number
+  return `${a} ${b} ${c} ${d} ${e}`;
+}
+
+const formatter = () => {
+  const formattedInput = formatInput(inputField.value);
+  inputField.value = formattedInput;
+}
+
+// displays result on the screen
+btn.addEventListener('click', () => {
+  let text = document.createElement('h1');
+  text.innerHTML = inputField.value;
+  result.appendChild(text);
+}
+)
