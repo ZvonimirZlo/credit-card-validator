88e2e1514e4447da33c67de20b3391decc881b69
ZvonimirZlo <lovrez2@gmail.com>
2024-04-22 15:03:59 +0200

    styles and res update

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const result = document.getElementById('result');
 const formatInput = () => {
   const nums = inputField.value.toString().replace(/[^\d]/g, '');
 
-  //slices the string in to pieces to form card number
+  //slices the input value to pieces to create card number form
   const partOne = nums.slice(0, 4);
   const partTwo = nums.slice(4, 8);
   const partThree = nums.slice(8, 12)
@@ -24,14 +24,14 @@ const formatter = () => {
 
 // displays result on the screen
 btn.addEventListener('click', () => {
- if(inputField.value){
-  result.innerHTML = `<p class="valid"> √ ${inputField.value} is a valid Credit Card/Debit Card Number.</p>`
+ if(inputField.value.length < 3){
+  result.innerHTML = `<h4 class="valid"> √ ${inputField.value} is a valid Credit Card/Debit Card Number.</h4><br>
+                      <p class='valid'>  √   The card has a correct length.</p>
+                      <p class="valid">  √   The card has a valid checksum.<p>
+                      <p class="valid">  √   The card brand code (Amex) is valid.</p>`
  }else{
-  result.innerHTML = `<p class="invalid">X Invalid input, enter full card number.</p>`
+  result.innerHTML = `<h4 class="invalid">X Invalid input, enter full card number.</h4>`
  }
-
-
-  
 }
 )
 
